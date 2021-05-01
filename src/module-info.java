e5847978fc7b6ec77b7a2ea3a276c2d578e09837
houda jouhar <jouharhouda2001@gmail.com>
2021-05-01 14:54:41 +0000

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module JavaFX.Database.Connectivity.master {
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires javafx.swing;
+    requires java.datatransfer;
+    requires java.xml;
+
+    opens javafx_db;
+}
